664ed856d4a0a30f2723014d5d7d36e2918b59b0
loonacuse <loonacuse@gmail.com>
2021-07-05 21:46:52 +0500

    Stack VM example (Bytecode pattern)

--- Loonge.Interpreter/Program.cs
+++ Loonge.Interpreter/Program.cs
@@ -4,9 +4,21 @@ namespace Loonge.interpreter
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var vm = new VirtualMachine();
+            vm.Interpret(new []
+            {
+                0, 5,   // Literal 5
+                0, 10,  // Literal 10
+                3,      // Add
+                5,      // GetTop
+                0, 15,
+                0, 25,
+                2
+            });
         }
     }
 }
---
+++ Loonge.Interpreter/VirtualMachine.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Loonge.interpreter
+{
+    public enum Instruction : int
+    {
+        Literal = 0,
+        WriteNum = 1,
+        WriteStr = 2,
+        Add = 3,
+        GetRegister = 4,
+        GetTop
+    }
+
+    public static class VmOperations
+    {
+        public static void WriteSomethingWithNumber(int message, int number)
+        {
+            Console.WriteLine($"Message: {message}. Number: {number}");
+        }
+
+        public static void WriteSomethingWithString(int message, int str)
+        {
+            Console.WriteLine($"Message: {message}. String: {str}");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class VirtualMachine
+    {
+        public static readonly int MaxStack = 128;
+        private int _stackSize = 0;
+        private int[] _stack;
+
+        private int _register = 0xF1;
+
+        public VirtualMachine()
+        {
+            _stack = new int[MaxStack];
+        }
+
+        public void Interpret(int[] bytecode)
+        {
+            for (var i = 0; i < bytecode.Length; i++)
+            {
+                var c = bytecode[i];
+
+                if ((Instruction) c == Instruction.WriteNum)
+                {
+                    var str = Pop();
+                    var str2 = Pop();
+
+                    VmOperations.WriteSomethingWithNumber(str, str2);
+                }
+                else if ((Instruction) c == Instruction.WriteStr)
+                {
+                    var str = Pop();
+                    var str2 = Pop();
+
+                    VmOperations.WriteSomethingWithString(str, str2);
+                }
+                else if ((Instruction) c == Instruction.Add)
+                {
+                    var str = Pop();
+                    var str2 = Pop();
+                    Push(VmOperations.Add(str, str2));
+                }
+                else if ((Instruction) c == Instruction.Literal)
+                {
+                    var value = bytecode[++i];
+                    Push(value);
+                }
+                else if ((Instruction) c == Instruction.GetRegister)
+                {
+                    Push(_register);
+                }
+                else if ((Instruction) c == Instruction.GetTop)
+                {
+                    Console.WriteLine(Pop());
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private void Push(int value)
+        {
+            Debug.Assert(_stackSize < MaxStack);
+            _stack[_stackSize++] = value;
+        }
+
+        private int Pop()
+        {
+            Debug.Assert(_stackSize > 0);
+            return _stack[--_stackSize];
+        }
+
+        private int Peek()
+        {
+            Debug.Assert(_stackSize > 0);
+            return _stack[_stackSize];
+        }
+    }
+}
